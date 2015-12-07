ea629b3b1cf72c4d9436a8b7c7e19b5f3fa2db94
shainanigans <me@shainakoval.com>
2015-12-07 11:20:40 +1100

    Undo reorganisation of folder

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      autoprefixer:{
+        dist:{
+          files:{
+            'css/style.css':'/css/style.css'
+          }
+        }
+      },
+      inlinecss: {
+        main: {
+            options: {
+            },
+            files: {
+                'index.html': 'index-build.html'
+            }
+        }
+    },
+      responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: "small",
+            width: 100,
+            quality: 80
+          },{
+            name: "medium",
+            width: 320,
+            quality: 80
+          },{
+            name: "large",
+            width: 720,
+            quality: 80
+          }]
+        },
+
+        /* Where to get and put images */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'views/images',
+          dest: 'views/images'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['views/images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['views/images']
+        },
+      },
+    },
+    uglify: {
+        build: {
+            src: 'js/perfmatters.js',
+            dest: 'js/perfmatters.min.js'
+        }
+    }
+    });
+    /* CSS Tasks */
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-inline-css');
+    /* Image Tasks */
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-mkdir');
+    /* JS Tasks */
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    /* Default Task */
+    grunt.registerTask('default', ['autoprefixer', 'clean', 'mkdir', 'responsive_images']);
+}
